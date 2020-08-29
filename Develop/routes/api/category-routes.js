2d59a5847c35faada9d5f9f1f3b9cab031930749
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-08-29 15:54:02 -0400

    tested routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -62,7 +62,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destory()
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
   .then(dbCategory => {
     if (!dbCategory) {
       res.status(404).json({ message: 'No user found with this id' });
