a2355f02fc7680289b52e6242fc45e85fbf2a4e9
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-08-29 14:56:31 -0400

    tested routes for put and add

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -54,7 +54,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-Product.create(req.body, {
+Product.create({
   product_name: req.body.product_name,
   price: req.body.price,
   stock: req.body.stock,
@@ -63,7 +63,7 @@ Product.create(req.body, {
 })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -119,7 +119,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+     console.log(err);
       res.status(400).json(err);
     });
 });
